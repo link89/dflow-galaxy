5e3c96269a02c0d9ef0e0d6f179e0836f3c7d0b7
weihong.xu <xuweihong.cn@gmail.com>
2024-02-23 20:33:38 +0800

    implement model_devi

--- dflow_galaxy/workflow/tesla/domain/model_devi.py
+++ dflow_galaxy/workflow/tesla/domain/model_devi.py
@@ -1,20 +1,27 @@
 from typing import List, Optional, Mapping, Any, Literal, Tuple
+from collections import namedtuple
 from dataclasses import dataclass
+from pathlib import Path
+import glob
+import os
+import re
+
+from joblib import Parallel, delayed
+from tabulate import tabulate
+import pandas as pd
+import ase.io
 
 from dflow_galaxy.core.pydantic import BaseModel
-from typing import Literal
 
-from dflow_galaxy.core.dispatcher import BaseApp, PythonApp, create_dispatcher, ExecutorConfig
+from dflow_galaxy.core.dispatcher import PythonApp, create_dispatcher, ExecutorConfig
 from dflow_galaxy.core.dflow import DFlowBuilder
-from dflow_galaxy.core.util import bash_iter_ls_slice
 from dflow_galaxy.core import types
 
-import pandas as pd
-
+from ai2_kit.core.util import load_text
 
 class ModelDeviConfig(BaseModel):
     metric: Literal["max_devi_v",  "min_devi_v",  "avg_devi_v",  "max_devi_f",  "min_devi_f",  "avg_devi_f"] = 'max_devi_f'
-    decent: Tuple[float, float]
+    decent_range: Tuple[float, float]
 
 
 @dataclass(frozen=True)
@@ -23,13 +30,73 @@ class ModelDeviArgs:
     work_dir: types.OutputArtifact
 
 
+_ModelDeviResult = namedtuple('_ModelDeviResult', ['data_dir', 'decent_files', 'ancestor', 'total', 'n_good', 'n_decent', 'n_bad'])
+
 class RunModelDeviTasksFn:
 
-    def __init__(self, config: ModelDeviConfig):
+    def __init__(self, config: ModelDeviConfig, workers: int, type_map: List[str]):
         self.config = config
+        self.workers = workers
+        self.type_map = type_map
 
     def __call__(self, args: ModelDeviArgs):
-        ...
+        data_dirs = glob.glob(f'{args.explore_dir}/tasks/*')
+        results: List[_ModelDeviResult] = Parallel(n_jobs=self.workers)(
+            delayed(self._process_lammps_dir)(Path(d) for d in data_dirs)
+        ) # type: ignore
+
+        # process results
+        headers = ['src', 'total', 'good', 'decent', 'bad', 'good%', 'decent%', 'poor%']
+        rows = [ ]
+        _pp = lambda a, b: f'{a / b * 100:.2f}%'
+        for r in results:
+            row = [r.data_dir, r.total, r.n_good, r.n_decent,
+                   _pp(r.n_good, r.total), _pp(r.n_decent, r.total), _pp(r.n_bad, r.total)]
+            rows.append(row)
+
+        report_text = tabulate(rows, headers=headers, tablefmt='tsv')
+
+
+
+    def _process_lammps_dir(self, data_dir: Path):
+        col = self.config.metric
+
+        model_devi_file = data_dir / 'model_devi.out'
+        traj_dir = data_dir / 'traj'
+
+        with open(model_devi_file, 'r') as f:
+            f.seek(1)  # skip the first letter '#'
+            df = pd.read_csv(f, delim_whitespace=True)
+        lo, hi = self.config.decent_range
+
+        good_df = df[df[col] < lo]
+        decent_df = df[(df[col] >= lo) & (df[col] < hi)]
+        bad_df = df[df[col] > hi]
+
+        traj_files = glob.glob(f'{traj_dir}/*.lammpstrj')
+        assert traj_files, f'no traj files is found in {traj_dir}'
+
+        traj_files = sorted(traj_files, key=_get_lammpstraj_frame_no)  # align
+
+        decent_files = [traj_files[i] for i in decent_df.index]
+
+        return _ModelDeviResult(
+            data_dir=data_dir,
+            decent_files=decent_files,
+            ancestor=load_text(data_dir / 'ANCESTOR'),
+            total=len(df),
+            n_good=len(good_df),
+            n_decent=len(decent_df),
+            n_bad=len(bad_df),
+        )
+
+
+
+def _get_lammpstraj_frame_no(filename):
+    m = re.match(r'^(\d+)', filename)
+    assert m, f'unexpected lammpstraj file name: {filename}'
+    return int(m.group(1))
+
 
 
 def provision_model_devi(builder: DFlowBuilder, ns: str, /,
@@ -37,6 +104,6 @@ def provision_model_devi(builder: DFlowBuilder, ns: str, /,
                          executor: ExecutorConfig,
                          python_app: PythonApp,
                          ):
-    run_task_fn = RunModelDeviTasksFn(config)
+    run_task_fn = RunModelDeviTasksFn(config, workers=python_app.max_worker)
 
 
--- tests/tesla.py
+++ tests/tesla.py
@@ -1,5 +1,6 @@
-from dflow_galaxy.workflow.tesla.domain import deepmd
+from dflow_galaxy.workflow.tesla.domain import deepmd, model_devi
 from dflow_galaxy.core.util import ensure_str
+from dflow_galaxy.core.dispatcher import Resource
 
 
 import unittest
@@ -7,18 +8,25 @@ import unittest
 
 class TestTesla(unittest.TestCase):
     def test_deepmd_train(self):
+        res = Resource()
+        deepmd_app = deepmd.DeepmdApp(resource=res)
         step = deepmd.RunDeepmdTrainingFn(
             config=deepmd.DeepmdConfig(),
-            concurrency=1,
-            dp_cmd='dp',
+            context=deepmd_app,
         )
         bash_script = step(deepmd.RunDeepmdTrainingArgs(
+            slice_idx='{{item}}',
             init_dataset='init-dataset',
-            iter_index=0,
+            iter_dataset='iter-dataset',
             work_dir='task_dir',
             persist_dir='output_dir',
         ))
         print(ensure_str(bash_script))
+
+
+    def test_get_lammpstraj_frame_no(self):
+        self.assertEqual(model_devi._get_lammpstraj_frame_no('100.lammpstraj'), 100)
+
 
 
 
